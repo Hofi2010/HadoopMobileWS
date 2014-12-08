baf4f94f0df37713cc164f803ae78af0b6a690e5
Klaus <khofenbitzer@gmail.com>
2014-12-08 06:06:49 -0500

    cleanup repro

--- HDFSFileStatus.m
+++
@@ -1,9 +0,0 @@
-//
-//  HDFSFileStatus.m
-//  HadoopConnect
-//
-//  Created by KLAUS HOFENBITZER on 12/7/14.
-//  Copyright (c) 2014 KLAUS HOFENBITZER. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
